bcb44aa625454510a9b075947d4850e9e8c787a1
Yubin <iamyubinkarki@gmail.com>
2024-05-28 17:06:42 +0545

    Layout and activity for 2nd unit

---
+++ app/src/main/java/com/bca/mobile_programming/unit_2/Unit_2.java
@@ -0,0 +1,15 @@
+// Declaring current package name
+package com.bca.mobile_programming.unit_2;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.bca.mobile_programming.R;
+
+public class Unit_2 extends Activity {
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.unit_2_hello);
+    }
+}
