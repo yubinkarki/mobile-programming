1dd66fab3a11ccf1a5a9ee39e9bab05ee4995444
Yubin <iamyubinkarki@gmail.com>
2024-06-25 19:33:17 +0545

    Change mindSdk to 29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.bca.mobile_programming"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
