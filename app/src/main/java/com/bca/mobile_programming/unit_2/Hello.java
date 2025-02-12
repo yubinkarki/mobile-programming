d85ba061d6f194bc9070aab9ab20ba9fefd07b34
Yubin <iamyubinkarki@gmail.com>
2025-02-12 09:20:00 +0545

    Remove getResource call

--- app/src/main/java/com/bca/mobile_programming/unit_2/Hello.java
+++ app/src/main/java/com/bca/mobile_programming/unit_2/Hello.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.content.res.Resources;
 
 import com.bca.mobile_programming.R;
 
@@ -15,8 +14,6 @@ public class Hello extends Activity {
         super.onCreate(b);
         setContentView(R.layout.unit_2_hello);
 
-        Resources res = getResources();
-
         TextView headingText = findViewById(R.id.helloHeadingText);
         Button submitButton = findViewById(R.id.helloSubmitButton);
         EditText fullNameInput = findViewById(R.id.helloFullNameInput);
@@ -24,7 +21,7 @@ public class Hello extends Activity {
         submitButton.setOnClickListener(v -> {
             String inputValue = fullNameInput.getText().toString();
 
-            if (inputValue.isEmpty()) headingText.setText(res.getString(R.string.na));
+            if (inputValue.isEmpty()) headingText.setText(R.string.na);
             else headingText.setText(inputValue);
         });
     }
