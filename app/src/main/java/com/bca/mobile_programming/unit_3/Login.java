ff7649afaf75a64a0220be8484ea3015da6df323
Yubin <iamyubinkarki@gmail.com>
2025-03-05 08:52:36 +0545

    Activity file for login screen

---
+++ app/src/main/java/com/bca/mobile_programming/unit_3/Login.java
@@ -0,0 +1,14 @@
+package com.bca.mobile_programming.unit_3;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.bca.mobile_programming.R;
+
+public class Login extends Activity {
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.unit_3_login);
+    }
+}
