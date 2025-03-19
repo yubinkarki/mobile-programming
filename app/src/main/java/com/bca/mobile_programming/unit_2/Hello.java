8d94904c97d941598784ef1af6ed44870b5d34d3
yubinkarki <iamyubinkarki@gmail.com>
2025-03-19 13:20:30 +0545

    Send data back to previous activity

--- app/src/main/java/com/bca/mobile_programming/unit_2/Hello.java
+++ app/src/main/java/com/bca/mobile_programming/unit_2/Hello.java
@@ -3,6 +3,7 @@ package com.bca.mobile_programming.unit_2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.Button;
+import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,13 +17,28 @@ public class Hello extends Activity {
 
         TextView headingText = findViewById(R.id.helloHeadingText);
         Button submitButton = findViewById(R.id.helloSubmitButton);
+        Button cancelButton = findViewById(R.id.helloCancelButton);
         EditText fullNameInput = findViewById(R.id.helloFullNameInput);
 
         submitButton.setOnClickListener(v -> {
             String inputValue = fullNameInput.getText().toString();
 
-            if (inputValue.isEmpty()) headingText.setText(R.string.na);
-            else headingText.setText(inputValue);
+            if (inputValue.isEmpty()) {
+                headingText.setText(R.string.na);
+            } else headingText.setText(inputValue);
         });
+
+        // Sending result back to the previous activity
+        cancelButton.setOnClickListener(v -> {
+            String sendBackData = fullNameInput.getText().toString();
+
+            if(sendBackData.isEmpty()) sendBackData = "Default Value";
+
+            Intent i = new Intent();
+            i.putExtra("contactData", sendBackData);
+            setResult(RESULT_OK, i);
+            finish();
+        });
+
     }
 }
