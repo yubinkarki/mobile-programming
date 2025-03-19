ccfc3cb1b1f720b9f76096d9421986bc5b66a2fa
yubinkarki <iamyubinkarki@gmail.com>
2025-03-19 13:19:44 +0545

    New activity for columns layout

---
+++ app/src/main/java/com/bca/mobile_programming/unit_3/Columns.java
@@ -0,0 +1,14 @@
+package com.bca.mobile_programming.unit_3;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.bca.mobile_programming.R;
+
+public class Columns extends Activity {
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.unit_3_columns);
+    }
+}
