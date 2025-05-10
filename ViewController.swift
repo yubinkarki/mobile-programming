8b632b29e0149d1c303267af77e8c6f89f7c188c
yubinkarki <iamyubinkarki@gmail.com>
2025-05-10 09:14:17 +0545

    Dismiss keyboard on outside tap

--- ViewController.swift
+++ ViewController.swift
@@ -1,16 +1,24 @@
+//
+//  ViewController.swift
+//  mobile-programming-ios
+//
+//  Created by Yubin on 5/3/25.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
+    // Hold the control button and drag the widget here
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var firstNumField: UITextField!
     @IBOutlet var secondNumField: UITextField!
 
     @IBAction func addButton(_ sender: Any) {
         var result: Double = 0;
-        let firstValue = Double(firstNumField.text!)
-        let secondValue = Double(secondNumField.text!)
+        let firstValue = Double(firstNumField.text!) ?? 0.0
+        let secondValue = Double(secondNumField.text!) ?? 0.0
 
-        result = firstValue! + secondValue!
+        result = firstValue + secondValue
 
         let formattedResult = String(format: "%.2f", arguments: [result])
 
@@ -19,6 +27,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Create a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+
+        // Make it not interfere with other controls
+        tapGesture.cancelsTouchesInView = false
+
+        view.addGestureRecognizer(tapGesture)
     }
+
+    @objc func dismissKeyboard() {
+        // Resign first responder status from any first responder (typically the active keyboard)
+        view.endEditing(true)
+       }
 }
