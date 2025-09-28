f53e671c1257963b595797ca130332c3cc179ba5
yubinkarki <iamyubinkarki@gmail.com>
2025-09-28 15:50:23 +0545

    Bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.bca.mobile_programming"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.bca.mobile_programming"
